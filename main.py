bf59c5d866138e96f56ee82726e8eac2f8a05eca
Laucian1 <resmithjr88@gmail.com>
2025-02-08 23:51:08 -0700

    Added report functionality

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def main():
         text = get_book_text(book_path)
         words = get_word_count(text)
         character_count = get_character_count(text)
-        print(f"Word count: {words}")
-        print(character_count)
-   
+        report = prepare_report(character_count)
+        print(f"--- Begin report of {book_path} ---")
+        print(f"{words} words found in the document\n")
+        print(report)
+        print("--- End report ---")
 
 def get_book_text(path):
         with open(path) as f:
@@ -25,5 +27,21 @@ def get_character_count(text):
                 char_dict[char] = 1
         return char_dict
                 
-                
+def prepare_report(char_dict):
+        dict_list = []
+        for entry in char_dict:
+            if entry.isalpha(): 
+                temp_dict = {"Character": entry, "number": char_dict.get(entry)}                 
+                dict_list.append(temp_dict)
+        dict_list.sort(reverse=True, key=sort_on)
+        meat = ""
+        for i in dict_list:
+              character = i.get("Character")
+              number = i.get("number")
+              meat += f"The {character} character was found {number} times\n"
+        return meat
+
+def sort_on(dict):
+      return dict["number"]
+          
 main()
