957297d567a119d0dd734c35e743e17c9a854caf
Laucian1 <resmithjr88@gmail.com>
2025-02-08 22:50:22 -0700

    Added character count functionality

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
         book_path = "books/frankenstein.txt"
         text = get_book_text(book_path)
         words = get_word_count(text)
-        print(text)
+        character_count = get_character_count(text)
         print(f"Word count: {words}")
+        print(character_count)
    
 
 def get_book_text(path):
@@ -14,4 +15,15 @@ def get_word_count(text):
         words = text.split()
         return len(words)
 
+def get_character_count(text):
+        char_dict = {}
+        lower_text = text.lower()
+        for char in lower_text:
+            if char in char_dict:
+                char_dict[char] +=1
+            else:
+                char_dict[char] = 1
+        return char_dict
+                
+                
 main()
